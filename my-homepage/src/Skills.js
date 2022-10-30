3ef54b92058a021d6e2ca23b65c95d4859cf2206
Shawen Aldred Harker <harkershawen@gmail.com>
2022-10-30 12:52:26 +0200

    Skills Icons Updated

--- my-homepage/src/Skills.js
+++ my-homepage/src/Skills.js
@@ -1,14 +1,17 @@
 import React from "react";
-import { VscTools } from "react-icons/vsc"
-import { AiOutlineHtml5 } from "react-icons/ai";
-import { TbBrandCss3, TbBrandBootstrap } from "react-icons/tb";
+import {
+  TbTools,
+  TbBrandHtml5,
+  TbBrandCss3,
+  TbDeviceMobile,
+  TbBrandJavascript,
+  TbBrandReactNative,
+  TbBrandBootstrap,
+  TbBrandUbuntu
+} from "react-icons/tb";
 import { GiWireframeGlobe } from "react-icons/gi";
-import { CiMobile4 } from "react-icons/ci";
-import { IoLogoJavascript } from "react-icons/io";
-import { FaReact } from "react-icons/fa";
 import { SiExpress, SiMaterialui } from "react-icons/si";
 import { DiPostgresql } from "react-icons/di";
-import { RiUbuntuLine } from "react-icons/ri";
 
 const Skills = () => {
   return (
@@ -16,14 +19,14 @@ const Skills = () => {
       <div className="container heading skills">
         <h2>My Skills</h2>
         <h2 className="icon">
-          <VscTools />
+          <TbTools />
         </h2>
       </div>
       <div>
         <h3 className="sub-heading">Technical Skills</h3>
         <uL>
           <li>
-            <AiOutlineHtml5 className="skills-icon" />
+            <TbBrandHtml5 className="skills-icon" />
             HTML5
           </li>
           <li>
@@ -35,15 +38,15 @@ const Skills = () => {
             Wireframing
           </li>
           <li>
-            <CiMobile4 className="skills-icon" />
+            <TbDeviceMobile className="skills-icon" />
             Mobile first
           </li>
           <li>
-            <IoLogoJavascript className="skills-icon" />
+            <TbBrandJavascript className="skills-icon" />
             JavaScript
           </li>
           <li>
-            <FaReact className="skills-icon" />
+            <TbBrandReactNative className="skills-icon" />
             React.js
           </li>
           <li>
@@ -63,7 +66,7 @@ const Skills = () => {
             Postgres SQL
           </li>
           <li>
-            <RiUbuntuLine className="skills-icon" />
+            <TbBrandUbuntu className="skills-icon" />
             Linux - Ubuntu
           </li>
         </uL>
